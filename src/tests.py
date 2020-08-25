8d631247438ffd8d90b58b42ba513b526f37da72
andre-silva-14 <silva.andre1997@gmail.com>
2020-08-25 23:33:06 +0200

    Update Run test workflow:
    - Runtime calculation has been moved to helpers.py
    - Time limits have now been added to the test suite.
    - Improved feedback given to user during test runtime.

--- src/tests.py
+++ src/tests.py
@@ -1,33 +1,33 @@
 from main import compiler
-import time
+from helpers import get_runtime
 
 
 def main():
     for index, test in enumerate(TESTS):
         print(f"\n\nStarting Test {index + 1}...")
 
-        program, expected_result = test
-        assert run_test(program.splitlines(), expected_result)
+        program, expected_result, time_limit = test
+        assert run_test(program.splitlines(), expected_result, time_limit)
 
 
-def run_test(program: list, expected: dict):
-    start = time.time()
-    result = compiler(program)
-    end = time.time()
-    runtime = round((end - start)* 1000)
+def run_test(program: list, expected: dict, time_limit: int):
+    print(f"Expected results: {expected}")
+    print(f"Maximum time limit: {f'{time_limit} ms' if time_limit else 'Infinite'}")
 
-    if result == expected:
-        print(f"Completed in {runtime} ms.")
-        print("Test completed successfully!")
+    result, runtime = get_runtime(compiler, program)
+    print(f"Actual results: {result}")
+    print(f"Completed in {runtime} ms.")
+
+    if result == expected and runtime <= time_limit:
+        print("\nTest successfully completed!")
         return True
     else:
-        print(f"Completed in {runtime} ms.")
-        print("Test Failed!")
+        print("\nTest Failed!")
         return False
 
 
 TESTS = [
-
+    # ( Test(str), Expected(dict), Limit Time(int // ms))
     (
         '''\
         trigger error
@@ -37,7 +37,8 @@ TESTS = [
         dec
         jnz
         inc ''',
-        {}
+        {},
+        2
     ),
 
     (
@@ -48,7 +49,8 @@ TESTS = [
         dec a
         jnz a -1
         inc a''',
-        {'a': 1}
+        {'a': 1},
+        1
     ),
 
     (
@@ -64,7 +66,8 @@ TESTS = [
         jnz c -5
         jnz 0 1
         mov c a''',
-        {'a': 409600, 'c': 409600, 'b': 409600}
+        {'a': 409600, 'c': 409600, 'b': 409600},
+        2000
     ),
 ]
 
